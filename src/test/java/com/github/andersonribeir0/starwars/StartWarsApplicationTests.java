908d7989fdc765c8bcbff99f2abbaecf15a42dcb
Anderson Ribeiro <anderson.moouro@gmail.com>
2019-10-02 00:43:14 -0300

    Initial commit

---
+++ src/test/java/com/github/andersonribeir0/starwars/StartWarsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.andersonribeir0.starwars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartWarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
